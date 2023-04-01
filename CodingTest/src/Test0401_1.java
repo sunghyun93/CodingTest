25c1b323c7ff2f59f9d29bf18f53aa2128bd29c5
sunghyun93 <107449729+sunghyun93@users.noreply.github.com>
2023-04-01 20:24:41 +0900

    4월1일 추억점수(쉽게 풀수 있을줄 알았는데 약간 헤맴...) map을 잘사용해야함

---
+++ CodingTest/src/Test0401_1.java
@@ -0,0 +1,43 @@
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class Test0401_1 {
+	public static int [] solution(String[] name, int[] yearning, String[][] photo) {
+        Map<String,Integer> map = new HashMap<>();
+        int [] answer = new int [photo.length];
+ 
+        
+
+        	for(int i=0; i < yearning.length; i++) {
+        		map.put(name[i],yearning[i]);
+        	}
+        	
+        	 for(int i=0; i < photo.length; i++) {
+             	for(int j=0; j < photo[i].length; j++) {
+             		if(map.containsKey(photo[i][j])) {
+             			answer[i]+=map.get(photo[i][j]);
+             		}
+             	}
+             }
+        
+        
+       
+       
+        	
+   
+        
+        return answer;
+    }
+	
+
+	public static void main(String[] args) {
+		String [] name = {"may", "kein", "kain", "radi"};
+		int [] yearning = {5, 10, 1, 3};
+		String[][] photo = {{"may", "kein", "kain", "radi"},{"may", "kein", "brin", "deny"},{"kon", "kain", "may", "coni"}};
+		
+		System.out.println(solution(name,yearning,photo));
+	}
+	
+}
